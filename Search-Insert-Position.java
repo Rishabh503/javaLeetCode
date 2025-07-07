008d2d9913c7eb3f9a0ef320aa763c688fcf9373
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2025-07-07 21:58:16 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 92.34%] - LeetPush

---
+++ Search-Insert-Position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] array, int target) {
+         int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if the target is present at mid
+            if (array[mid] == target) {
+                return mid; // Target found, return index
+            }
+
+            // If target is greater, ignore left half
+            if (array[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+        return left+(right-left)/2; // Target not found
+    }
+
+}
