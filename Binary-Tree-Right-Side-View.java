c04d4064b6196d8e3f4b6018c116ec06e8de660a
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2025-07-26 13:07:11 +0530

    [199] [Time Beats: 100.00%] [Memory Beats: 62.43%] - LeetPush

--- Binary-Tree-Right-Side-View.java
+++ Binary-Tree-Right-Side-View.java
@@ -17,19 +17,34 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
         if(root==null) return ans;
-        Queue<TreeNode> qt=new LinkedList<>();
-        qt.add(root);
-        while(!qt.isEmpty()){
-            int size=qt.size();
-            for(int i=0;i<size;i++){
-                TreeNode currentNode=qt.poll();
-                if(i==size-1){
-                    ans.add(currentNode.val);
-                }
-                if(currentNode.left!=null) qt.add(currentNode.left);
-                if(currentNode.right!=null) qt.add(currentNode.right);
-            }
-        }
+        ans.add(root.val);
+        int maxLevel[]=new int[1];
+        rightView(root,0,maxLevel,ans);
         return ans;
+        // Queue<TreeNode> qt=new LinkedList<>();
+        // qt.add(root);
+        // while(!qt.isEmpty()){
+        //     int size=qt.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode currentNode=qt.poll();
+        //         if(i==size-1){
+        //             ans.add(currentNode.val);
+        //         }
+        //         if(currentNode.left!=null) qt.add(currentNode.left);
+        //         if(currentNode.right!=null) qt.add(currentNode.right);
+        //     }
+        // }
+        // return ans;
     }
+    public void rightView(TreeNode root,int currentLevel,int maxLevel[],List<Integer> ans){
+        if(root==null) return;
+
+        if(currentLevel>maxLevel[0]){
+            ans.add(root.val);
+            maxLevel[0]=currentLevel;
+        }
+        rightView(root.right,currentLevel+1,maxLevel,ans);
+        rightView(root.left,currentLevel+1,maxLevel,ans);
+    }
+
 }
