50b46fafc56f8c8ccb95b4ed3116a0ee74ed07fa
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2025-10-24 12:37:53 +0530

    [41] [Time Beats: 11.97%] [Memory Beats: 96.42%] - LeetPush

---
+++ First-Missing-Positive.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(num>0) set.add(num);
+        }
+        for(int i=1;i<=100001;i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
