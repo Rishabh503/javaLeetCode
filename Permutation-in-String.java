5ffbe1401487c3e5f063021948021ad08462a8fc
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2025-11-01 15:19:35 +0530

    [567] [Time Beats: 32.71%] [Memory Beats: 7.27%] - LeetPush

--- Permutation-in-String.java
+++ Permutation-in-String.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        // ek hashmap bana lo
+        // then window banao 
+        // agr us window me jo freq hai wo same hai in both the maps then ans is true
+        // else its false
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character,Integer> map2=new HashMap<>();
         for(int i=0;i<s1.length();i++){
             char ch=s1.charAt(i);
-           map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
         }
         int i=0;
         int j=0;
         int ws=s1.length();
         while(j<s2.length()){
+            //window banao
             char ch=s2.charAt(j);
             map2.put(ch,map2.getOrDefault(ch,0)+1);
+            //window ghatao
             if(j-i+1>ws){
                 char chi=s2.charAt(i);
                 map2.put(chi, map2.get(chi) - 1);
@@ -20,8 +26,8 @@ class Solution {
             }
 
             if(j-i+1==ws && map1.equals(map2)) return true;
+            //kam kro
             j++;
-
         }
 
 
