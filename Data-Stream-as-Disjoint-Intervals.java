85c603f1427c5555ff32e4fcb334a985fa319efe
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2026-01-19 21:53:13 +0530

    [352] [Time Beats: 7.72%] [Memory Beats: 54.35%] - LeetPush

--- Data-Stream-as-Disjoint-Intervals.java
+++ Data-Stream-as-Disjoint-Intervals.java
@@ -1,7 +1,7 @@
 1class SummaryRanges {
-2    List<Integer> list;
+2    TreeSet<Integer> list;
 3    public SummaryRanges() {
-4        list=new ArrayList<Integer>();
+4        list=new TreeSet<Integer>();
 5    }
 6    
 7    public void addNum(int value) {
@@ -14,45 +14,41 @@
 14        if(list.size()==0){
 15            return new int[][]{};
 16        }
-17        //sort the list 
-18        Collections.sort(list);
-19
-20        // take 2 iterators/pointer 
-21        Integer left=null,right=null;
-22
-23        List<int[]> res=new ArrayList<>();
-24
-25        for(int i=0;i<list.size();i++){
-26            if(left==null && right==null){
-27                // res.add(new int[]{})
-28                left=list.get(i);
-29                right=list.get(i);
-30            }else if(list.get(i)==right+1){
-31                right=list.get(i);
-32            }else{
-33                res.add(new int[]{
-34                    left,right
-35                });
-36                left=list.get(i);
-37                right=list.get(i);
-38            }
-39        }
-40        res.add(new int[]{
-41            left,right
-42        });
-43
-44        // make the desired ans/
-45        int ans[][]=new int[res.size()][2];
-46        for(int i=0;i<res.size();i++){
-47            ans[i]=res.get(i);
-48        }
-49        return ans;
-50    }
-51}
-52
-53/**
-54 * Your SummaryRanges object will be instantiated and called as such:
-55 * SummaryRanges obj = new SummaryRanges();
-56 * obj.addNum(value);
-57 * int[][] param_2 = obj.getIntervals();
-58 */
+17        Integer left=null,right=null;
+18
+19        List<int[]> res=new ArrayList<>();
+20
+21        for(Integer val:list){
+22            if(left==null && right==null){
+23                // res.add(new int[]{})
+24                left=val;
+25                right=val;
+26            }else if(val==right+1){
+27                right=val;
+28            }else{
+29                res.add(new int[]{
+30                    left,right
+31                });
+32                left=val;
+33                right=val;
+34            }
+35        }
+36        res.add(new int[]{
+37            left,right
+38        });
+39
+40        // make the desired ans/
+41        int ans[][]=new int[res.size()][2];
+42        for(int i=0;i<res.size();i++){
+43            ans[i]=res.get(i);
+44        }
+45        return ans;
+46    }
+47}
+48
+49/**
+50 * Your SummaryRanges object will be instantiated and called as such:
+51 * SummaryRanges obj = new SummaryRanges();
+52 * obj.addNum(value);
+53 * int[][] param_2 = obj.getIntervals();
+54 */
