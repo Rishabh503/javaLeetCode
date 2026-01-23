19a99b46219569cfad3a3ccc01579397d6c9d44f
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2026-01-24 02:36:39 +0530

    [1337] [Time Beats: 92.52%] [Memory Beats: 98.34%] - LeetPush

--- The-K-Weakest-Rows-in-a-Matrix.java
+++ The-K-Weakest-Rows-in-a-Matrix.java
@@ -1,25 +1,30 @@
 1class Solution {
 2    public int[] kWeakestRows(int[][] mat, int k) {
 3        // queue <soliders count , row index > 
-4        // using minheap direclty 
+4        // using maxheap  
 5        PriorityQueue<int[]> pq=new PriorityQueue<>(
 6            (a,b)->{
-7                if(a[0]!=b[0]) return a[0]-b[0];
-8                return a[1]-b[1];
+7                if(a[0]!=b[0]) return b[0]-a[0];
+8                return b[1]-a[1];
 9            }
 10        );
-11        for(int i=0;i<mat.length;i++){
-12            int c=0;
-13            for(int j=0;j<mat[i].length;j++){
-14                if(mat[i][j]==1)c++;
-15                else break;
-16            }
-17            pq.add(new int[]{c,i});
-18        }
-19        int ans[]=new int[k];
-20        for(int i=0;i<k;i++){
-21            ans[i]=pq.poll()[1];
-22            }
-23            return ans;
-24    }
-25}
+11        // tc  O(n*(m+logn))
+12        for(int i=0;i<mat.length;i++){
+13            int c=0;
+14            for(int j=0;j<mat[i].length;j++){
+15                if(mat[i][j]==1)c++;
+16                else break;
+17            }
+18            pq.add(new int[]{c,i});
+19            if(pq.size()>k){
+20                pq.poll();
+21            }
+22        }
+23        int[] ans = new int[k];
+24        for (int i = k - 1; i >= 0; i--) {
+25            ans[i] = pq.poll()[1];
+26        }
+27
+28        return ans;
+29    }
+30}
