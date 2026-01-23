5c2bd5068f191f542c8ad2d85193658a197e00bc
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2026-01-24 01:00:06 +0530

    [347] [Time Beats: 94.76%] [Memory Beats: 40.99%] - LeetPush

--- Top-K-Frequent-Elements.java
+++ Top-K-Frequent-Elements.java
@@ -1,43 +1,43 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        //bucket sort lagyeyge because alone hashmap is faling
-        // lets make the bucket and hasmap
-        List<Integer>[] bucket=new List[nums.length+1];
-        Map<Integer,Integer> map=new HashMap<>();
-
-        // fill the elements 
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-
-        //fill the buckets
-
-        // iterate over the hashmap right 
-        for(int key:map.keySet()){
-            // find the element 
-            int element=map.get(key);
-            //check if the bucket for it exists or not 
-            if(bucket[element]==null){
-                bucket[element]=new ArrayList<>();
-            }
-            // add the element 
-            bucket[element].add(key);
-        }
-        // ans ke liye array n counter
-       int result[]=new int[k];
-       int counter=0;
-
-       for(int i=bucket.length-1;i>=0 && counter<k;i--){
-        // check krna pdega na bucket pe saman hai ya n ii 
-        if(bucket[i]!=null){
-            // bucket ek arraylist h uska saman hume result me dalna 
-            // hai bascially agre ek frequeny k pass ek se jyada element hue to wo aise hi daldege 
-            for(Integer j:bucket[i]){
-                result[counter++]=j;
-            }
-        }
-       }
-        return result;
-
-    }
-}
+1class Solution {
+2    public int[] topKFrequent(int[] nums, int k) {
+3        //bucket sort lagyeyge because alone hashmap is faling
+4        // lets make the bucket and hasmap
+5        List<Integer>[] bucket=new List[nums.length+1];
+6        Map<Integer,Integer> map=new HashMap<>();
+7
+8        // fill the elements 
+9        for(int num:nums){
+10            map.put(num,map.getOrDefault(num,0)+1);
+11        }
+12
+13        //fill the buckets
+14
+15        // iterate over the hashmap right 
+16        for(int key:map.keySet()){
+17            // find the element 
+18            int element=map.get(key);
+19            //check if the bucket for it exists or not 
+20            if(bucket[element]==null){
+21                bucket[element]=new ArrayList<>();
+22            }
+23            // add the element 
+24            bucket[element].add(key);
+25        }
+26        // ans ke liye array n counter
+27       int result[]=new int[k];
+28       int counter=0;
+29
+30       for(int i=bucket.length-1;i>=0 && counter<k;i--){
+31        // check krna pdega na bucket pe saman hai ya n ii 
+32        if(bucket[i]!=null){
+33            // bucket ek arraylist h uska saman hume result me dalna 
+34            // hai bascially agre ek frequeny k pass ek se jyada element hue to wo aise hi daldege 
+35            for(Integer j:bucket[i]){
+36                result[counter++]=j;
+37            }
+38        }
+39       }
+40        return result;
+41
+42    }
+43}
