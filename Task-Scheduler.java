9278a984b74d18fcb776d4820ccb161382a3c5f7
Rishabh503 <145251387+Rishabh503@users.noreply.github.com>
2026-02-27 21:11:42 +0530

    [621] [Time Beats: 50.24%] [Memory Beats: 78.81%] - LeetPush

--- Task-Scheduler.java
+++ Task-Scheduler.java
@@ -1,21 +1,45 @@
-1class Solution {
-2
-3    public int leastInterval(char[] tasks, int n) {
-4        HashMap<Character, Integer> map = new HashMap<>();
-5        int maxfreq = -1;
-6        for (int i = 0; i < tasks.length; i++) {
-7            map.put(tasks[i], map.getOrDefault(tasks[i], 0) + 1);
-8            maxfreq = Math.max(map.get(tasks[i]), maxfreq);
-9        }
-10        int countMax = 0;
-11        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-12            if (maxfreq == entry.getValue()) {
-13                countMax++;
-14            }
-15        }
-16
-17        int time = (maxfreq - 1) * (n + 1) + countMax;
-18
-19        return Math.max(time, tasks.length);
-20    }
-21}
+1class Task{
+2    int freq;
+3    int time;
+4
+5    public Task(int f,int t){
+6        this.freq=f;
+7        this.time=t;
+8    }
+9}
+10
+11class Solution {
+12    public int leastInterval(char[] tasks, int n) {
+13        int time=0;
+14
+15        PriorityQueue<Task> maxHeap=new PriorityQueue<>((a,b)->b.freq-a.freq);
+16        Queue<Task> qt=new LinkedList<>();
+17
+18        int [] counts=new int [26];
+19
+20        for(char t:tasks){
+21            counts[t-'A']= counts[t-'A']+1;
+22        }
+23        for(int c:counts){
+24            if(c>0){
+25                maxHeap.add(new Task(c,1));
+26            }
+27        }
+28
+29        while(!maxHeap.isEmpty() || !qt.isEmpty()){
+30            time=time+1;
+31            while(!qt.isEmpty() && qt.peek().time==time){
+32                maxHeap.add(qt.poll());
+33            }
+34            if(!maxHeap.isEmpty()){
+35                Task taskToExecute=maxHeap.poll();
+36                taskToExecute.freq=taskToExecute.freq-1;
+37                taskToExecute.time=time+(n+1);
+38                if(taskToExecute.freq>0){
+39                    qt.add(new Task(taskToExecute.freq,taskToExecute.time));
+40                }
+41            }
+42        }
+43return time ;
+44    }
+45}
